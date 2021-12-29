e71181dff52e87919dccff0cc23003b25f8d9f08
fsdr <filipsordyl8@gmail.com>
2021-12-29 20:41:11 +0100

    better version

--- seeds/index.js
+++ seeds/index.js
@@ -1,33 +1,32 @@
-const mongoose = require("mongoose");
-const cities = require("./cities");
-const { places, descriptors } = require("./seedHelper");
 const Place = require("../models/place");
-async function main() {
-  await mongoose.connect(
-    "mongodb+srv://fsdr:dupadupa@cluster0.gafjh.mongodb.net/Cluster0?retryWrites=true&w=majority"
-  );
-}
-
+const cities = require("./cities");
+// mongoose and mongo set up data
+const mongoose = require("mongoose");
 main()
   .then(() => {
     console.log("connected");
   })
   .catch(err => console.log(err));
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://fsdr:dupadupa@cluster0.gafjh.mongodb.net/Cluster0?retryWrites=true&w=majority"
+  );
+}
 
-const seedDB = async () => {
+async function clearDB() {
   await Place.deleteMany({});
-  for (let i = 0; i < 50; i++) {
-    const random1000 = Math.floor(Math.random() * 1000);
-    const place = new Place({
-      location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`,
-    });
+}
+
+async function seedDB() {
+  for (let city in cities) {
+    const place = new Place({ title: city.city });
+
     await place.save();
   }
-};
+}
 
+clearDB();
 seedDB().then(() => {
   mongoose.connection.close();
 });
