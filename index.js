b1d7c9d1cd567458cb213a5b5026b1dc97d5757b
fsdr <filipsordyl8@gmail.com>
2021-12-29 19:08:58 +0100

    initial main app setup

---
+++ index.js
@@ -0,0 +1,34 @@
+// express set up
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const methodOverride = require("method-override");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// mongoose and mongo set up data
+const mongoose = require("mongoose");
+main()
+  .then(() => {
+    console.log("connected");
+  })
+  .catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://fsdr:dupadupa@cluster0.gafjh.mongodb.net/Cluster0?retryWrites=true&w=majority"
+  );
+}
+
+app.get('/', (req, res) => {
+    res.render("home")
+})
+
+// express server is on port 3000 - >  localhost:3000
+app.listen(3000, () => {
+  console.log(">> listening on port 3000");
+});
