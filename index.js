70f6f30f158e96de8290cd38c28d9d6dd3be79db
fsdr <filipsordyl8@gmail.com>
2021-12-30 11:11:36 +0100

    complete Crud

--- index.js
+++ index.js
@@ -44,11 +44,35 @@ app.post("/places/", async (req, res) => {
   await place.save();
   res.redirect(`places/${place.id}`);
 });
+
 app.get("/places/:id", async (req, res) => {
   const { id } = req.params;
   const place = await Place.findById(id);
   res.render("places/show", { place });
 });
+
+app.get("/places/:id/edit", async (req, res) => {
+  const trimmed = req.params.id.trim();
+  const place = await Place.findById(trimmed);
+  res.render("places/edit", { place });
+});
+
+app.put("/places/:id", async (req, res) => {
+  const { id } = req.params;
+  const place = await Place.findByIdAndUpdate(
+    id,
+    { title: req.body.place.title },
+    { runValidators: true }
+  );
+  res.redirect(`/places/${place._id}`);
+});
+
+app.delete("/places/:id", async (req, res) => {
+  const { id } = req.params;
+  await Place.findByIdAndDelete(id);
+  res.redirect("/places");
+});
+
 // express server is on port 3000 - >  localhost:3000
 app.listen(3000, () => {
   console.log(">> listening on port 3000");
